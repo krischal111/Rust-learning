ab5b6247ff812d306267f1d35cc6c607fd839029
Krischal Khanal <krischal111@gmail.com>
2022-06-06 23:36:12 +0545

    Optimized to the moon

--- challenges/project-euler/largest_prime_factor.rs
+++ challenges/project-euler/largest_prime_factor.rs
@@ -1,8 +1,15 @@
 fn main() {
-    let a: i64 = 600851475143;
+    let num: i64 = 600851475143;
     let mut prime_list: Vec<i64> = vec![];
     let mut prime_factors: Vec<i64> = vec![];
-    for i in 2..=(((a*2) as f64).sqrt() as i64) {
+
+    let mut a = num;
+    let mut end = ((a * 2) as f64).sqrt() as i64;
+    let mut i = 2; 
+    loop {
+        if i > end {
+            break;
+        }
         let mut isprime = true;
         'primecheck: for prime_number in &prime_list {
             if (i % prime_number) == 0 {
@@ -11,12 +18,26 @@ fn main() {
             }
         }
         if isprime {
+            // println!("Prime number = {}", i);
             prime_list.push(i);
             if (a % i) == 0 {
                 prime_factors.push(i);
+                let mut b = a;
+                while (b%i) == 0 {
+                    println!("Prime factor = {}", i);
+                    b /= i;
+                    if (b % i) != 0 {
+                        a = b;
+                        break;
+                    }
+                }
             }
         }
+        end = ((a * 2) as f64).sqrt() as i64;
+        i += 1;
     }
-    println!("Prime number list is\n {:?}", prime_list);
-    println!("The prime factors of {a} are \n {:?}", prime_factors);
+    println!("Last prime factor = {}",a);
+    prime_factors.push(a);
+    println!("Prime numbers generated in the process = \n {:?}\nNumber of primes generated = {}",prime_list, prime_list.len());
+    println!("The prime factors of {} are \n {:?}",num , prime_factors);
 }
